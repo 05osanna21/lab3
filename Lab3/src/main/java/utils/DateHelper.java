955f6419453e5dc4a422f4cbbde4d07975f25d9c
andreydorokhin <andrey.dorokhin.dam@gmail.com>
2020-11-09 18:46:12 +0200

    Lab3Update

---
+++ Lab3/src/main/java/utils/DateHelper.java
@@ -0,0 +1,30 @@
+package utils;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+
+public class DateHelper {
+
+    public static DateTime getDateAsDaysFromNow(int fromNow) {
+        DateTime dateTime = new DateTime();
+        LocalDate localDate = dateTime.toLocalDate();
+        String localDate1 = localDate.minusDays(fromNow).toString();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd");
+        DateTime jodatime = dateTimeFormatter.parseDateTime(localDate1);
+        return jodatime;
+    }
+
+    public static String getDateInPropertlyFormat(DateTime dayFromNow) {
+        DateTimeFormatter dtfOut = DateTimeFormat.forPattern("dd/mm/yyyy");
+        return dtfOut.print(dayFromNow);
+    }
+
+    public static int getDayNumbeFromDate(DateTime jodatime) {
+        int dayOfMonth = jodatime.getDayOfMonth();
+        return dayOfMonth;
+    }
+
+}
--- Lab3/src/test/java/seleniumTest/InputForm.java
+++ Lab3/src/test/java/seleniumTest/InputForm.java
@@ -1,6 +1,8 @@
 package seleniumTest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.assertj.core.api.Assertions;
+import org.joda.time.DateTime;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,23 +11,20 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.Color;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import utils.DateHelper;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
-public class InputForm{;
+public class InputForm {
+    ;
     WebDriver driver;
     WebDriverWait wait;
 
 
     By inputFormOptionBy = By.xpath("//a[text()='Input Forms']");
-    By waitSimpleFormDemo= By.xpath("//li[@class='tree-branch']//a[text()='Simple Form Demo']");
+    By waitSimpleFormDemo = By.xpath("//li[@class='tree-branch']//a[text()='Simple Form Demo']");
     By userMessageBy = By.xpath("//input[@id='user-message']");
     By showButtonBy = By.xpath("//button[text()='Show Message']");
     By actualResualtTest_1By = By.xpath("//span[@id='display']");
@@ -33,7 +32,7 @@ public class InputForm{;
     By sum2By = By.xpath("//input[@id='sum2']");
     By getTotalBy = By.xpath("//button[text()='Get Total']");
     By actualResualtTest_2By = By.xpath("//span[@id='displayvalue']");
-    By waitCheckboxDemo= By.xpath("//li[@class='tree-branch']//a[text()='Checkbox Demo']");
+    By waitCheckboxDemo = By.xpath("//li[@class='tree-branch']//a[text()='Checkbox Demo']");
     By option_1By = By.xpath("//label[ text()='Option 1']");
     By option_3By = By.xpath("//label[ text()='Option 3']");
     By option = By.xpath("//label[ text()='Option 1' and text()='Option 3']");
@@ -47,30 +46,38 @@ public class InputForm{;
     By daySelectedBy = By.xpath("//p[text()='Day selected :- Friday']");
     By multiSelectBy = By.xpath("//select[@id='multi-select']");
     By getAllSelectedBy = By.xpath("//button[text()='Get All Selected']");
-    By optionSelectedAreBy = By.xpath("//p[text()='Options selected are : Florida,New York']");
+    By optionSelectedAreBy = By.xpath("//p[@class='getall-selected']");
     By waitAjaxFormSubmitBy = By.xpath("//li[@class='tree-branch']//a[text()='Ajax Form Submit']");
     By buttonSubmitBy = By.xpath("//input[@id='btn-submit']");
-    By colorValueBy = By.xpath("//input[@style='border: 1px solid rgb(255, 0, 0);']");
+    By colorValueBy = By.xpath("//input[@id='title']");
     By waitDatePickersBy = By.xpath("//li[@class='tree-branch']//a[text()='Date pickers']");
     By bootstrapDatePickerBy = By.xpath("//li[@class='tree-branch']//a[text()='Bootstrap Date Picker']");
-    By inputGroupAddonBy = By.xpath("//span[@class='input-group-addon']//i[@class='glyphicon glyphicon-th']");
-
+    By inputGroupAddonBy = By.xpath("//span[@class='input-group-addon']");
+    By waitTableBy = By.xpath("//li[@class='tree-branch']//a[text()='Table']");
+    By tableDataSearchBy = By.xpath("//ul[@class='dropdown-menu']//a[text()='Table Data Search']");
+    By actualBy = By.xpath("//div[@id='sandbox-container1']//input");
 
 
     @Before
     public void beforeEachTest() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
         driver.get("https://www.seleniumeasy.com/test/");
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, 5);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='No, thanks!']"))).click();
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='No, thanks!']"))).click();
+        } catch (Exception e) {
+
+        }
 
 
     }
 
     @Test
-    public void test_1(){
+    public void test_1() {
         String userMes = "Hello, my name is Osana";
         WebElement inputFormOption = driver.findElement(inputFormOptionBy);
         inputFormOption.click();
@@ -80,12 +87,13 @@ public class InputForm{;
         WebElement showButton = driver.findElement(showButtonBy);
         showButton.click();
         String actualResualt = driver.findElement(actualResualtTest_1By).getText();
-        Assert.assertEquals(userMes,actualResualt);
+        Assert.assertEquals(userMes, actualResualt);
 
 
     }
-   @Test
-    public void test_2(){
+
+    @Test
+    public void test_2() {
         String sum = "15";
         WebElement inputFormOption = driver.findElement(inputFormOptionBy);
         inputFormOption.click();
@@ -97,11 +105,12 @@ public class InputForm{;
         WebElement getTotal = driver.findElement(getTotalBy);
         getTotal.click();
         String actualResualt = driver.findElement(actualResualtTest_2By).getText();
-        Assert.assertEquals(sum,actualResualt);
+        Assert.assertEquals(sum, actualResualt);
 
     }
+
     @Test
-    public void test_3(){
+    public void test_3() {
         WebElement inputFormOption = driver.findElement(inputFormOptionBy);
         inputFormOption.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(waitCheckboxDemo)).click();
@@ -111,11 +120,12 @@ public class InputForm{;
         option_3.click();
         Boolean actualResult1 = driver.findElement(option_1By).isSelected();
         Boolean actualResult2 = driver.findElement(option_3By).isSelected();
-        Assert.assertTrue(actualResult1==actualResult2);
+        Assert.assertTrue(actualResult1 == actualResult2);
 
     }
+
     @Test
-    public void test_4(){
+    public void test_4() {
 
         WebElement inputFormOption = driver.findElement(inputFormOptionBy);
         inputFormOption.click();
@@ -130,10 +140,11 @@ public class InputForm{;
         //String Sex = "Sex : Female";
         //String Age = " Age group: 5 - 15";
         String actualResual = driver.findElement(male_15_50By).getText();
-        Assert.assertNotEquals(female_5_15,male_15_50By);
+        Assert.assertNotEquals(female_5_15, actualResual);
     }
+
     @Test
-    public void test_5(){
+    public void test_5() {
         WebElement inputFormOption = driver.findElement(inputFormOptionBy);
         inputFormOption.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(waitSelectDropdownListBy)).click();
@@ -143,10 +154,11 @@ public class InputForm{;
         WebElement dropdown = driver.findElement(dropdownBy);
         dropdown.click();
         String actualResualt = driver.findElement(daySelectedBy).getText();
-        Assert.assertEquals(daySelect,actualResualt);
+        Assert.assertEquals(daySelect, actualResualt);
     }
+
     @Test
-    public void test_6(){
+    public void test_6() {
         WebElement inputFormOption = driver.findElement(inputFormOptionBy);
         inputFormOption.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(waitSelectDropdownListBy)).click();
@@ -158,42 +170,59 @@ public class InputForm{;
         getAllSelected.click();
         String optionSelected = "Options selected are : Florida,New York";
         String actualResualt = driver.findElement(optionSelectedAreBy).getText();
-        Assert.assertEquals(optionSelected,actualResualt);
+        Assert.assertEquals(optionSelected, actualResualt);
     }
 
     @Test
-    public void test_7(){
+    public void test_7() {
         WebElement inputFormOption = driver.findElement(inputFormOptionBy);
         inputFormOption.click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(waitAjaxFormSubmitBy)).click();
         WebElement submitButton = driver.findElement(buttonSubmitBy);
         submitButton.click();
-        String expectedColor = "1px solid rgb(255, 0, 0)";
-        String actualResualt = driver.findElement(colorValueBy).getCssValue("border");
-        Assert.assertEquals(expectedColor,actualResualt);
+        String expectedColor = "255, 0, 0";
+        WebElement inputField = driver.findElement(colorValueBy);
+        String actualResualtColor = inputField.getAttribute("outerHTML");
+        Assertions.assertThat(actualResualtColor).contains(expectedColor);
+
     }
+
     @Test
-    public void test_8(){
+    public void test_8() {
         WebElement datePickers = driver.findElement(waitDatePickersBy);
         datePickers.click();
         WebElement bootstrapDatePicker = driver.findElement(bootstrapDatePickerBy);
         bootstrapDatePicker.click();
         WebElement inputGroupAddon = driver.findElement(inputGroupAddonBy);
         inputGroupAddon.click();
-        DateFormat dateFormat = new SimpleDateFormat("dd/MMM/yyyy");
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DATE, -1);
-        Date yesterday = calendar.getTime();
-        String expectedResualt = "Sat Nov 07 22:43:19 EET 2020";
-        String actualResual = new String(yesterday.toString());
-        Assert.assertEquals(expectedResualt,actualResual);
+
+        DateTime twoDaysFromToday = DateHelper.getDateAsDaysFromNow(2);
+        int dayNumbeFromDate = DateHelper.getDayNumbeFromDate(twoDaysFromToday);
+        WebElement calendarOpenButton = driver.findElement(By.xpath("//div[@class='datepicker-days']//td[text()='" + dayNumbeFromDate + "']"));
+        calendarOpenButton.click();
+        String actualResual = driver.findElement(actualBy).getText();
+        Assert.assertEquals(DateHelper.getDateInPropertlyFormat(twoDaysFromToday), actualResual);
+    }
+
+    @Test
+    public void test_9() throws InterruptedException {
+        WebElement table = driver.findElement(waitTableBy);
+        table.click();
+        Thread.sleep(1000);
+        WebElement tableDataSearch = driver.findElement(tableDataSearchBy);
+        Thread.sleep(1000);
+        tableDataSearch.click();
+        WebElement filter = driver.findElement(By.xpath("//input[@id='task-table-filter']"));
+        filter.sendKeys("Emily John");
+
 
 
     }
-   @After
-    public void closeBrowser(){
 
-       // driver.quit();
+    @After
+    public void closeBrowser() {
+
+        driver.quit();
     }
 
 }
